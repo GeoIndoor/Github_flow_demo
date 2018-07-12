904e8bd3984d01b10180dba9c8d39abcb03f2da3
whu_gj <30277881+Grace1126@users.noreply.github.com>
2018-07-13 04:03:29 +0800

    add-main
    add main

---
+++ hellomain/src/main/java/top/geomatics/Main.java
@@ -0,0 +1,12 @@
+package top.geomatics;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting greet = new GreetingImpl("Hello World");
+		greet.sayHello();
+
+	}
+
+}
